91548b24ac632e1d2042013ff46f61d15d3601d5
Zacoolak <118558953+Zacoolak@users.noreply.github.com>
2023-04-23 11:37:10 +0200

    Add files via upload

---
+++ Genarate.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import make_classification
+
+
+class Generate():
+    def generate(x):
+        n_samples = 12345
+        n_features = 2
+        n_redundant = 0
+        n_clusters_per_class = 1
+        flip_y = 0
+        random_state = 1337
+        X, y = make_classification(n_samples=n_samples, n_features=n_features, n_redundant=n_redundant,
+                                   n_clusters_per_class=n_clusters_per_class, weights=[x], flip_y=flip_y,
+                                   random_state=random_state)
+        return X, y
